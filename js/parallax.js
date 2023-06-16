08ddd7756379075a4840b2ff9373903292702fc5
pedro092692 <pedro_092692@hotmail.com>
2023-06-15 22:46:13 -0400

    initial import

---
+++ js/parallax.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+		var windowwidth = $(window).width();
+		if(windowwidth>800){
+			var scroll = $(window).scrollTop();
+
+			$('header .textos').css({
+				'transform': 'translate(0px,'+ scroll / 2 + '%)'
+			});
+
+			$('.acerca-de article').css({
+				'transform': 'translate(0px, -'+ scroll / 4 + '%)'
+			});
+
+		}
+	});
+
+		$(window).resize(function(){
+		var windowwidth = $(window).width();
+		if(windowwidth<800){
+			$('.acerca-de article').css({
+				'transform': 'translate(0px, 0px)'
+			});
+		}
+	});
+});
